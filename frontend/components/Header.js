11e577f81ac0ce5352c79860c20e6088443f34c4
john <johnjamero007@gmail.com>
2021-03-17 00:27:03 +0800

    stykes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,12 +1,54 @@
 import Link from "next/link";
+import styled from "styled-components";
 import Nav from "./Nav";
+
+
+const Logo = styled.h1`
+background: red;
+  color: white;
+  display: inline-block;
+  padding: 4px 5px;
+  transform: skew(-7deg);
+  margin: 0;
+  margin-left: 5rem;
+  margin-bottom:1rem;
+  position : relative;
+  z-inex:2;
+  font-size: 4rem;
+  border-radius: 5px;
+
+`;
+
+const HeaderStyles = styled.header`
+.bar{
+    border-bottom: 10px solid var(--black, black );
+    display: grid;
+    grid-template-columns : auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.sub-bar{
+
+    display: grid;
+    grid-template-columns : auto 1fr;
+    border-bottom: 1px solid var(--black, black );
+
+
+
+
+
+}
+`;
+
+
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
         <Link href="/">
-          <a>Sick fits</a>
+          <a><Logo >Super-me</Logo></a>
         </Link>
       </div>
       <div className="sub-bar">
@@ -14,6 +56,6 @@ export default function Header() {
       </div>
 
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,10 @@
-import Document, { Html, Head, NextScript, Main } from "next/document";
+import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        {/* <Head></Head> */}
+      <Html lang="en-CA">
+        <Head />
         <body>
           <Main />
           <NextScript />
